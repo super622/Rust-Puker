6e379aa0cbdcddcd97487a48d7d90a59e84e6c8a
YelloRose <masonrose622@gmail.com>
2022-02-14 19:51:39 +0200

    update tests

--- tests/test_utils.rs
+++ tests/test_utils.rs
@@ -4,6 +4,8 @@ use ggez::{
     graphics::{Rect},
 };
 
+const DELTA_TIME: f32 = 1. / 60.;
+
 #[test]
 fn test_circle_overlap() {
     let c1 = (Vec2::ZERO.into(), 5.);
@@ -33,10 +35,9 @@ fn test_dynamic_rect_vs_rect() {
     let d_rect = Rect::new(0., 1., 3., 2.);
     let d_vel = Vec2::X * 2.;
     let s_rect = Rect::new(2., 0.5, 2., 2.);
-    let delta_time = 1. / 60.;
     let (mut cp, mut cn, mut t) = (Vec2::ZERO, Vec2::ZERO, 0.);
 
-    assert!(dynamic_rect_vs_rect(&d_rect, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, delta_time));
+    assert!(dynamic_rect_vs_rect(&d_rect, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, DELTA_TIME));
 
     let new_pos = Vec2::new(d_rect.x, d_rect.y) + cn * d_vel.abs() * (1. - t);
     assert!(!s_rect.overlaps(&Rect::new(new_pos.x, new_pos.y, d_rect.w, d_rect.h)));
@@ -47,25 +48,23 @@ fn test_dynamic_circle_vs_rect() {
     let mut d_c = (Vec2::ZERO.into(), 2.);
     let d_vel = Vec2::X;
     let s_rect = Rect::new(1., 1., 2., 2.);
-    let delta_time = 1. / 60.;
     let (mut cp, mut cn, mut t) = (Vec2::ZERO, Vec2::ZERO, 0.);
 
-    assert!(dynamic_circle_vs_rect(&d_c, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, delta_time));
+    assert!(dynamic_circle_vs_rect(&d_c, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, DELTA_TIME));
 
     d_c = ((d_c.0.0 - cn * t).into(), d_c.1);
-    assert!(!dynamic_circle_vs_rect(&d_c, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, delta_time));
+    assert!(!dynamic_circle_vs_rect(&d_c, &d_vel, &s_rect, &mut cp, &mut cn, &mut t, DELTA_TIME));
 }
 
 #[test]
 fn test_dynamic_circle_vs_circle() {
-    let mut c1 = (Vec2::ZERO.into(), 2.);
-    let mut c2 = (Vec2::X.into(), 2.);
+    let c1 = (Vec2::ZERO.into(), 2.);
+    let c2 = (Vec2::X.into(), 2.);
     let mut vel1 = Vec2::X;
     let mut vel2 = -Vec2::X;
-    let delta_time = 1. / 60.;
     let (mut v1, mut v2) = (Vec2::ZERO, Vec2::ZERO);
 
-    assert!(dynamic_circle_vs_circle(&c1, &vel1, &c2, &vel2, &mut v1, &mut v2, delta_time));
+    assert!(dynamic_circle_vs_circle(&c1, &vel1, &c2, &vel2, &mut v1, &mut v2, DELTA_TIME));
 
     vel1 += v1;
     vel2 += v2;
